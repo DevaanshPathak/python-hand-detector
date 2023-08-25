74fdec9fb76f9c88cb42f9ea21d96d1a8608a965
Devaansh Pathak <pathakdevaansh2008@gmail.com>
2023-08-25 19:56:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,40 @@
-import cv2
 from cvzone.HandTrackingModule import HandDetector
-import numpy as np
-import math
-import time
+import cv2
 
-# Initialize the webcam capture
 cap = cv2.VideoCapture(0)
-
-# Initialize the hand detector with a maximum of one hand detection
-detector = HandDetector(maxHands=1)
-
-# Offset for cropping around the detected hand
-offset = 20
-
-# Desired size of the cropped hand image
-imgSize = 300
-
-# Folder path to save captured images
-folder = "Data/C"
-
-# Counter to keep track of saved images
-counter = 0
-
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 while True:
-    # Capture a frame from the webcam
+    # Get image frame
     success, img = cap.read()
-
-    # Detect hands in the captured frame
-    hands, img = detector.findHands(img)
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)  # with draw
+    # hands = detector.findHands(img, draw=False)  # without draw
 
     if hands:
-        # Get information about the first detected hand
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
 
-        # Create a white background image
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        fingers1 = detector.fingersUp(hand1)
 
-        # Crop the captured frame around the hand
-        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        if len(hands) == 2:
+            # Hand 2
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+            centerPoint2 = hand2['center']  # center of the hand cx,cy
+            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
 
-        # Calculate aspect ratio of the cropped region
-        aspectRatio = h / w
+            fingers2 = detector.fingersUp(hand2)
 
-        if aspectRatio > 1:
-            # Resize the cropped region with a vertical orientation
-            k = imgSize / h
-            wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
-        else:
-            # Resize the cropped region with a horizontal orientation
-            k = imgSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
-
-        # Display the cropped hand image
-        cv2.imshow("ImageCrop", imgCrop)
-
-        # Display the composite image with white background
-        cv2.imshow("ImageWhite", imgWhite)
-
-    # Display the original captured frame with detected hand bounding box
+            # Find Distance between two Landmarks. Could be same hand or different hands
+            length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
+            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
+    # Display
     cv2.imshow("Image", img)
-
-    # Check for key press event
-    key = cv2.waitKey(1)
-
-    if key == ord("s"):
-        # Save the composite image with a timestamped filename
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
-        print(counter)
+    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
